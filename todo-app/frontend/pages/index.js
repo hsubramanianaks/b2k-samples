1a3d3c158009bf8ccd7be872b378abdf46fe1356
Hariharan Subramanian <hsubramanian@microsoft.com>
2024-03-05 15:57:05 +0000

    move all projects from main repo to fork

---
+++ todo-app/frontend/pages/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import TodoApp from '../components/TodoApp'
+
+export default class Index extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <TodoApp />
+        );
+    }
+}
